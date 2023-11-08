f18b7ed0bf896142c0c799795f5fb9c47364ec6c
Arkadii Ivonin <ark811g@gmail.com>
2023-11-09 00:34:19 +0300

    new recording - Couner1.py

--- Couner1.py
+++ Couner1.py
@@ -1,4 +1,4 @@
-d = {}
+Total_lines = {}
 
 with open('1.txt') as a1:
     for count, line in enumerate(a1):
@@ -15,18 +15,12 @@ with open('1.txt') as a1:
             pass
     s3 = (count + 1)
 
-d = {"1.txt": s1, "2.txt": s2, "3.txt": s3}
-list_d = list(d.items())
+Total_lines = {"1.txt": s1, "2.txt": s2, "3.txt": s3}
+list_d = list(Total_lines.items())
 list_d.sort(key=lambda i: i[1])
 for k, v in list_d:
     print(k + ':', v)
     
-
-from fileinput import FileInput
-
-with FileInput(["1.txt", "2.txt", "3.txt"]) as input_files:
-    with open("All_.txt", "w") as output_file:
-        output_file.writelines(input_files)
 
 
 
