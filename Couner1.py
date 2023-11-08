5b1be954d55d4141d5e4af7c23d8330adc8d3f4b
Arkadii Ivonin <ark811g@gmail.com>
2023-11-09 01:41:17 +0300

    new recording - Couner1.py

--- Couner1.py
+++ Couner1.py
@@ -1,27 +1,36 @@
-Total_lines = {}
 
-with open('1.txt') as a1:
-    for count, line in enumerate(a1):
-        pass
+from fileinput import FileInput
+
+with FileInput(["1.txt", "2.txt", "3.txt"]) as input_files:
+    with open("All_.txt", 'w') as output_file:
+        output_file.writelines(input_files)
+
+with open("All_.txt", 'a') as f:
+    f.close()
+
+    with open('1.txt') as a1:
+          for count, line in enumerate(a1):
+                pass
     s1 = (count + 1)
     
-    with open('2.txt') as a2:
+with open('2.txt') as a2:
         for count, line in enumerate(a2):
             pass
-    s2 = (count + 1)
-    
-    with open('3.txt') as a3:
-        for count, line in enumerate(a3):
-            pass
-    s3 = (count + 1)
-
+        s2 = (count + 1)
+        
+with open('3.txt') as a3:
+            for count, line in enumerate(a3):
+                pass
+            s3 = (count + 1)
+            
 Total_lines = {"1.txt": s1, "2.txt": s2, "3.txt": s3}
 list_d = list(Total_lines.items())
 list_d.sort(key=lambda i: i[1])
 for k, v in list_d:
-    print(k + ':', v)
+    result = (k + ':', v)
+    x = (f'Служебная информация:', result)
+    print(x)
     
-
 
 
 
